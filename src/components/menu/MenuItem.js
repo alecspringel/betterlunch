f8dcfb753bfe9f9894c5e455a831efd04ffa1b5c
Alec Springel <alecspringel@gmail.com>
2022-08-13 20:02:49 -0700

    Updated ESLint config

--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -4,22 +4,21 @@ import styled from 'styled-components';
 
 const dietaryInfo = (data) => {
   if (data.vegetarian && data.glutenfree) {
-    return "VEG + GF"
-  } else if (data.vegetarian) {
-    return "VEG"
-  } else if (data.glutenfree) {
-    return "GF"
-  } else {
-    return null
+    return 'VEG + GF';
+  } if (data.vegetarian) {
+    return 'VEG';
+  } if (data.glutenfree) {
+    return 'GF';
   }
-}
+  return null;
+};
 
 function MenuItem({ image, data }) {
   const fetchedImage = getImage(image);
   const dietaryText = dietaryInfo(data);
   return (
     <Card>
-      <GatsbyImage image={fetchedImage} style={{ objectFit: 'cover', borderRadius: "6px 6px 0 0" }} />
+      <GatsbyImage image={fetchedImage} style={{ objectFit: 'cover', borderRadius: '6px 6px 0 0' }} />
       <DescriptionDiv>
         <p className="text-bold mont">
           {data?.title}
@@ -49,10 +48,3 @@ const Card = styled.article`
 const DescriptionDiv = styled.div`
   padding: 10px 15px;
 `;
-
-const Icon = styled.img`
-  width: 14px;
-  height: 14px;
-  margin: 0 10px;
-  vertical-align: middle;
-`
